18768e387aec530f637c2dd276e044ee0b47309f
youer0219 <xlm20040219@qq.com>
2025-02-13 17:22:40 +0800

    地图：实现地图边界显示节点并重构家院地图瓦片位置

---
+++ debug/map_border/map_border.gd
@@ -0,0 +1,19 @@
+@tool
+extends Node2D
+
+const MAX_HIGHT_CELLS := 24
+const MAX_LENTH_CELLS := 72
+
+@export var enable:bool = false:set = _set_enable
+
+func _draw() -> void:
+	if not enable:
+		return
+	
+	var rect:Rect2 = Rect2(Vector2.ZERO,LevelData.MAP_SIZE)
+	draw_rect(rect,Color.BROWN,false,3)
+
+func _set_enable(value:bool):
+	enable = value
+	
+	queue_redraw()
