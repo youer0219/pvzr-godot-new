a0871ad084e16d5e99c16fa5b143f71f55eaf3a4
youer0219 <xlm20040219@qq.com>
2025-02-13 23:08:05 +0800

    地图：使用地形重构梯子实现

--- scenes/map/ladder_map/ladder_map.gd
+++ scenes/map/ladder_map/ladder_map.gd
@@ -1,29 +1,12 @@
 class_name LadderMap
 extends TileMapLayer
 
-func _ready() -> void:
-	update_internals()  ## 强制更新。确保在初始的梯子节点ready之后调用ready函数。
-	_set_ladders()
-
-## TODO: GODOT中的地形机制是否可以无代码地完成梯子图像设置工作。目前的尝试并不成功。
-func _set_ladders():
-	var used_cells := get_used_cells()
-	
-	for ladder:Ladder in get_children():
-		var curr_cell := local_to_map(ladder.position)
-		var up_cell := curr_cell + Vector2i.UP
-		var down_cell := curr_cell + Vector2i.DOWN
-		
-		ladder.has_up = used_cells.has(up_cell)
-		ladder.has_down = used_cells.has(down_cell)
-
-## TODO: 是否存在更加优雅的对新增和删除的节点的处理方法
-# 指定 cell 生成ladder
 func create_ladder_by_cell(cell:Vector2i):
-	set_cell(cell,0,Vector2i.ZERO,1)
-	call_deferred("_set_ladders")
+	## TODO: 不能在有水的地方生成，所以可能需要一个检查
+	set_cell(cell,1,Vector2i(2,0))
+	set_cells_terrain_connect([cell],0,0)
 
-# 指定 cell 删除ladder
-func delete_ladder_by_cell(cell:Vector2i):
+func deleta_ladder_by_cell(cell:Vector2i):
+	## TODO: 不能把水删除，所以可能也需要一个检查
 	erase_cell(cell)
-	call_deferred("_set_ladders")
+	set_cells_terrain_connect([cell],0,-1) ## 通过设为-1将原本地形清除
