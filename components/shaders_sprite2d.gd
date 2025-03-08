07ce2643fcc0e043a0d847deeaec6a4820eec643
youer0219 <xlm20040219@qq.com>
2025-03-08 10:15:10 +0800

    组件：最终完善ShadersSprite2D组件

--- components/shaders_sprite2d.gd
+++ components/shaders_sprite2d.gd
@@ -1,15 +1,16 @@
 @tool
 class_name ShadersSprite2D
 extends Sprite2D
-## 使用方法：
-## 在bottom_texture中添加图像；在shaders_dir中添加着色器名称与着色器材质资源
-## 不要自己配置sprite2d的texture和material属性，会被替换！
-## 该节点在编辑器中也能实时显示效果。但目前存在因数据配置出错等问题导致图像不能正确显示的问题。
-## 可以在配置正确的数据后，点击generate按钮刷新一下
-## WARNING:这里允许创建CanvasItemMaterial类型的材质，但作者没有用过，所以没有测试……
-
-## 已知编辑器报错：ERROR: Path to node is invalid: 'Sprite2D/@SubViewport@23869'.
-## 但对于程序似乎没有影响
+## Multi-pass shader processor for Sprite2D with real-time editor preview.
+## https://github.com/youer0219/ShadersSprite2D/tree/main
+## [br][br]Usage:
+## - Set base texture in 'bottom_texture'
+## - Add shader materials to 'shaders_dic' (key: shader name, value: material)
+## - Click generate button to refresh effects
+## [br][br]Warnings:
+## - Do NOT manually set texture/material properties - they will be overwritten
+## - Only supports CanvasItemMaterial/ShaderMaterial types
+## - Editor preview may glitch with invalid configurations
 
 @export_tool_button("generate") var genarete_func = generate
 @export var bottom_texture:Texture2D:
@@ -23,11 +24,12 @@ extends Sprite2D
 		generate()
 		update_configuration_warnings()
 
-var first_sub_viewport:SubViewport
+var first_sub_viewport:SubViewport # Root viewport for effect chain
 
 func _ready() -> void:
 	generate()
 
+## Clears all generated nodes and resets state
 func clear():
 	texture = null
 	material = null
@@ -35,18 +37,21 @@ func clear():
 		first_sub_viewport.queue_free()
 		first_sub_viewport = null
 
+## Updates shader parameter for specified material
 func set_shader_param_by_name(choose_material_name:StringName,param:StringName, value:Variant):
 	var choose_material = get_material_by_name(choose_material_name) as ShaderMaterial
 	if choose_material == null:
 		return null
 	choose_material.set_shader_parameter(param,value)
 
+## Retrieves material from dictionary by name
 func get_material_by_name(choose_material_name:StringName)->Material:
 	if not shaders_dic.has(choose_material_name):
 		push_warning(name + " shaders_dir" + "not has "+ choose_material_name)
 		return null
 	return shaders_dic[choose_material_name]
 
+## Generates multi-pass shader effect chain
 func generate():
 	clear()
 	
@@ -55,8 +60,9 @@ func generate():
 	
 	var shaders_dic_size := shaders_dic.size()
 	
-	## 没有shader，返回
+	## no shader and return
 	if shaders_dic_size == 0:
+		texture = bottom_texture
 		return
 	
 	var last_sprite_2d:Sprite2D = self
@@ -71,23 +77,25 @@ func generate():
 		if shaders_dic_size - index == 1:
 			last_sprite_2d.texture = bottom_texture
 		else:
-			## 添加subviewport
+			## add subviewport
 			var new_subviewport:SubViewport = _get_subviewport()
 			last_sprite_2d.add_child(new_subviewport)
+			
+			## node first subviewport
 			if first_sub_viewport == null:
 				first_sub_viewport = new_subviewport
 			
-			## 给sprite2d添加viewport-texture
+			## add viewport-texture for last sprite2d
 			last_sprite_2d.texture = new_subviewport.get_texture()
 			
-			## 添加新的sprite2d
+			## add new sprite2d
 			var new_sprite_2d:Sprite2D = Sprite2D.new()
 			new_sprite_2d.position = bottom_texture.get_size() / 2.0
 			new_subviewport.add_child(new_sprite_2d)
 			last_sprite_2d = new_sprite_2d
 
 
-## 内部方法。获取一个适配图像大小的禁用3D渲染的透明背景的SubViewport实例化节点。
+## Creates configured SubViewport matching texture size
 func _get_subviewport()->SubViewport:
 	var subviewport = SubViewport.new()
 	subviewport.disable_3d = true
@@ -100,18 +108,28 @@ func _get_configuration_warnings():
 	var warnings = []
 	
 	if shaders_dic.size() <= 1:
-		## 不建议在没有或只有一个shader的情况下使用该节点
 		warnings.append("It is not recommended to use this node when there is no shader or only one shader.")
 	
 	var shader_materals := shaders_dic.values()
 	for shader_materal in shader_materals:
 		if not (shader_materal.is_class("ShaderMaterial") or shader_materal.is_class("CanvasItemMaterial")):
-			## 请保证字典中的材质为ShaderMaterial或CanvasItemMaterial类型
-			warnings.append("Please ensure that the materials in the dictionary are of the ShaderMaterial or CanvasItemMaterial type.")
+			warnings.append("Only ShaderMaterial/CanvasItemMaterial supported.")
 			break
 	
 	if bottom_texture == null:
-		## 没有图像，shader资源无法生效
-		warnings.append("Without an image, the shader resource will not take effect.")
+		warnings.append("No bottom image provided; shaders won't take effect.")
 	
 	return warnings
+
+## Delete the texture before saving, and assign the value again after saving 
+## to avoid the editor error by saving the automatically generated viewport texture to the scene file('Path to node is invalid')
+## The solution comes from:
+## https://forum.godotengine.org/t/how-to-make-variables-for-scripts-running-in-the-editor-not-save-to-the-scene-file-in-godot/104490/2
+## Thanks for the guidance:https://forum.godotengine.org/u/mrcdk/summary
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_EDITOR_PRE_SAVE:
+			texture = null
+		NOTIFICATION_EDITOR_POST_SAVE:
+			if first_sub_viewport != null:
+				texture = first_sub_viewport.get_texture()
