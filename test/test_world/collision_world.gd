8755e64bff1a3c5348c6a525d1fc68a0baed5f31
youer0219 <xlm20040219@qq.com>
2025-06-29 20:37:56 +0800

    重新开始制作PVZR

--- test/test_world/collision_world.gd
+++ test/test_world/collision_world.gd
@@ -1,28 +1,3 @@
 extends Node2D
 
-@onready var entity: RigidBody2D = $Entity
 @onready var map: Map = $Map
-
-var bullet_modifiers:Array[BulletModifier]
-
-func _ready() -> void:
-	bullet_modifiers.append(TorchStumpBulletModifier.new())
-	bullet_modifiers.append(FireTailBulletModifier.new())
-	#await get_tree().create_timer(1.5).timeout
-	#
-	#var damage_data:DamageData = preload("res://data/damage_data/test_damage_data.tres").duplicate()
-	#damage_data.damage_type = DamageData.DamageType.EXPLOSIVE_DAMAGE
-	#for i in range(10):
-		#damage_data.damage = 5.0
-		#await get_tree().create_timer(0.4).timeout
-		#entity.apply_damage(damage_data)
-
-
-func _on_timer_timeout() -> void:
-	pass
-	#var new_path := map.map_path_finder.get_global_path(get_global_mouse_position(),entity.global_position)
-	#PathShowTool.draw_path(self,new_path,Color.AZURE,5.0)
-
-func _on_bullet_finder_body_entered(body: Node2D) -> void:
-	if body is Bullet:
-		BulletModifier.apply_bullet_modifiers(body,bullet_modifiers)
