c13d81161cc4714d75544b0ce57c9d6ed999d144
youer0219 <xlm20040219@qq.com>
2025-07-16 17:33:27 +0800

    实体：添加状态机调试UI

--- move_entity/dave/crazy_dave.gd
+++ move_entity/dave/crazy_dave.gd
@@ -4,6 +4,7 @@ class_name CrazyDave
 ## TODO:要重写气球相关方法，因为戴夫可能不会在触碰天花板后立即退出该模式
 ## 而是在退出hurt模式后一并退出气球模式。同时倒地等功能是否重合呢？
 
+
 func _lengthwise_input_handle():
 	if Input.is_action_just_pressed("move_up"):
 		is_just_move_up = true
