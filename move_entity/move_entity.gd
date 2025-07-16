5e149cd0447e0c8c9e57d115c34bdad00ab4d865
youer0219 <xlm20040219@qq.com>
2025-07-16 23:18:43 +0800

    实体移动：优化躺下状态时水中漂浮效果

--- move_entity/move_entity.gd
+++ move_entity/move_entity.gd
@@ -154,11 +154,13 @@ func _on_lay_state_state_entered() -> void:
 	var tween := create_tween()
 	tween.tween_property(visual_control,"rotation_degrees",90 * entity_dir,0.5)
 	collision_shape_2d.shape.height *= 0.5
+	char_move.char_move_data.water_sink_distance *= 2.0
 
 func _on_lay_state_state_exited() -> void:
 	var tween := create_tween()
 	tween.tween_property(visual_control,"rotation_degrees",0,0.5)
 	collision_shape_2d.shape.height *= 2.0
+	char_move.char_move_data.water_sink_distance *= 0.5
 
 func _on_in_ground_check_area_body_entered(_body: Node2D) -> void:
 	entity_chart.send_event("emerge_ground")
