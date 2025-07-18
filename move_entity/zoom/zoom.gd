fb9099b440de4bdbedc4e2d336521b39fe389b3e
youer0219 <xlm20040219@qq.com>
2025-07-18 23:17:55 +0800

    僵尸：追逐代码优化

--- move_entity/zoom/zoom.gd
+++ move_entity/zoom/zoom.gd
@@ -11,6 +11,7 @@ const MOVEMENT_THRESHOLD := 8
 
 var target:Node2D ## 考虑单独一个节点或模块来获取target，并使用静态变量避免重复获取
 var last_lateral_move_direction:int = 0
+var curr_path:Array[Vector2]
 
 func _ready() -> void:
 	super()
@@ -25,22 +26,23 @@ func _on_chase_state_state_physics_processing(_delta: float) -> void:
 		push_error("target == null")
 		return
 	
-	var curr_path = entity_path_finder.get_map_path_to_global_pos(target.global_position)
+	## TODO:直接多帧更新一次，有时僵尸移动会左右摇摆，可能需要一个“到达机制”
+	## 如有合适的“到达机制”，可以每0.1s才更新一次路径
+	if get_tree().get_frame() % 2 == 0:
+		curr_path = entity_path_finder.get_map_path_to_global_pos(target.global_position)
 	
-	if curr_path.size() <= 1:
-		return
-	
-	## 横向移动
-	if abs(curr_path[1].x - global_position.x) > MOVEMENT_THRESHOLD:
-		lateral_move_direction = signi(int(curr_path[1].x - global_position.x))
-		last_lateral_move_direction = lateral_move_direction
-	else:
-		lateral_move_direction = last_lateral_move_direction
-	
-	## 纵向移动 
-	if curr_path[1].y - global_position.y < 0 + entity_pos_deviation:
-		is_just_move_up = true
-		is_move_up = true
+	if curr_path.size() >= 2:
+		## 横向移动
+		if abs(curr_path[1].x - global_position.x) > MOVEMENT_THRESHOLD:
+			lateral_move_direction = signi(int(curr_path[1].x - global_position.x))
+			last_lateral_move_direction = lateral_move_direction
+		else:
+			lateral_move_direction = last_lateral_move_direction
+		
+		## 纵向移动 
+		if curr_path[1].y - global_position.y < 0 + entity_pos_deviation:
+			is_just_move_up = true
+			is_move_up = true
 	
 	if not attack_check_area.bodys.is_empty():
 		entity_chart.send_event("attack")
