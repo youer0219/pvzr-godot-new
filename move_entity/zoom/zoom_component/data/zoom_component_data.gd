672d3948468e18110348420b90c5ad60ae780a0e
youer0219 <xlm20040219@qq.com>
2025-07-22 11:14:21 +0800

    僵尸：组件系统框架搭建

---
+++ move_entity/zoom/zoom_component/data/zoom_component_data.gd
@@ -0,0 +1,21 @@
+extends Resource
+class_name ZoomComponentData
+
+var actions:ZoomComponentActions = ZoomComponentActions.new()
+
+
+## 添加组件时效果
+func _on_add_zoom_component():
+	pass
+
+## 移除组件时效果
+func _on_remove_zoom_component():
+	pass
+
+## 自然死亡效果
+func _on_common_dead():
+	pass
+
+## 灰烬死亡效果
+func _on_ash_dead():
+	pass
