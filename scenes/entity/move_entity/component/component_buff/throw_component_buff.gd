61cd02dab81dd352d99880221d523dfa2f4b7cea
youer0219 <xlm20040219@qq.com>
2025-07-31 12:25:44 +0800

    组件系统：修复同帧添加的组件抛出buff无法生效bug

--- scenes/entity/move_entity/component/component_buff/throw_component_buff.gd
+++ scenes/entity/move_entity/component/component_buff/throw_component_buff.gd
@@ -3,11 +3,18 @@ class_name ThrowComponentBuff
 
 @export var throw_force:Vector2 = Vector2(80,-200)
 
-func _on_buff_start(_container: GD_BuffContainer, runtime_buff: GD_RuntimeBuff) -> void:
+func _init() -> void:
+	stack_type = STACK_TYPE.STACK
+
+func _on_buff_awake(_container: GD_BuffContainer, runtime_buff: GD_RuntimeBuff) -> void:
+	_throw_component(runtime_buff)
+
+func _on_stack_layer_change(_container: GD_BuffContainer,runtime_buff: GD_RuntimeBuff,_new_runtime_buff: GD_RuntimeBuff,_is_over:bool):
+	## 不考虑_is_over时，只要有新buff就会触发该方法，不需担心layer上限
+	_throw_component(runtime_buff)
+
+func _throw_component(runtime_buff:GD_RuntimeBuff)->void:
 	var component = runtime_buff.blackboard["component"] as EntityComponent
 	var force = runtime_buff.buff.throw_force
 	var damage_data = runtime_buff.blackboard["damage_data"]
 	EntityComponentActions.throw_component(damage_data,component,force)
-
-## TODO: 同帧添加的buff无法生效（name重复）
-## 解决方法： name唯一化  //  使用堆叠，但这可能需要await a frame
--- scenes/entity/move_entity/component/entity_component_data/entity_component_data.gd
+++ scenes/entity/move_entity/component/entity_component_data/entity_component_data.gd
@@ -1,11 +1,10 @@
 # EntityComponentData.gd (组件数据资源)
-@tool
 class_name EntityComponentData
 extends Resource
 
 @export_group("基础设置")
 @export var init_hp: float = 5.0
-@export var component_texture: Texture2D:set = _set_component_texture
+@export var component_texture: Texture2D
 @export var component_type: EntityComponent.EntityComponentType = EntityComponent.EntityComponentType.BODY
 
 @export_group("策略配置")
@@ -13,7 +12,3 @@ extends Resource
 @export var component_dead_buffs:Array[GD_Buff]
 @export var common_dead_buffs:Array[GD_Buff]
 @export var is_offset_damage:bool = false
-
-func _set_component_texture(value:Texture2D):
-	component_texture = value
-	emit_changed()
